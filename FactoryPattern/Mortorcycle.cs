78d5c726d67f2f00c9b59caabdc617cb7be99838
Celeste McCall <celestemccall93@gmail.com>
2023-02-09 20:15:15 -0600

    initial commit

---
+++ FactoryPattern/Mortorcycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FactoryPattern
+{
+	public class Mortorcycle : IVehicle
+	{
+		public Mortorcycle()
+		{
+		}
+
+        public void Drive()
+        {
+            Console.WriteLine("This motorycle is able to drive on and off the road!");
+        }
+    }
+}
+
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace FactoryPattern
+{
+	public static class VehicleFactory
+	{
+		public static IVehicle GetVehicle(int wheelCount)
+		{
+			switch (wheelCount)
+			{
+				case 2:
+					return new Mortorcycle();
+				case 4:
+					return new Car();
+				case 3:
+					return new Mortorcycle();
+				default:
+					return new Car();
+
+
+			}
+		}
+
+    }
+}
+
